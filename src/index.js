e568b2fc1a75e17a3767cb04b48928075f7a4941
Amelie Pira <amelie.pira@gmail.com>
2022-06-10 20:48:49 +0100

    added and checked npm run start

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//requiring the inquirer module
+const inquirer = require("inquirer");
+
+// specify the app to open in
+//open("http://localhost:port/filename", { app: "chrome" });
+
+//main function
+const init = () => {
+  console.log("hello");
+};
+
+init();
