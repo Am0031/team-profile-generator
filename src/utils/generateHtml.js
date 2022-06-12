d2fad50854d33f230a133bdd5dca35dd45f50584
Amelie Pira <amelie.pira@gmail.com>
2022-06-12 17:39:06 +0100

    completed strings in generateHtml

--- src/utils/generateHtml.js
+++ src/utils/generateHtml.js
@@ -1,126 +1,5 @@
 const generateHtml = (manager, team) => {
-  console.log(manager);
-  console.log(team);
-
-  const teamName = manager.team;
-
-  const alertMessage = `<div class="warning-message alert-warning w-100 mt-2 mb-2 text-center">
-    No engineers
-  </div>`;
-
-  const engineerCard = `<div class="card employee-card m-2">
-<div class="card-body bg-engineer">
-  <h5 class="card-title">John</h5>
-  <p class="card-subtitle">
-    <i class="fa-solid fa-brain"></i> Engineer
-  </p>
-</div>
-<ul class="list-group list-group-flush p-2 list-container">
-  <li class="list-group-item">ID: 41</li>
-  <li class="list-group-item d-flex flex-row">
-    <p class="link-text pr-2">
-      <i class="fa-solid fa-envelope"></i> Email:
-    </p>
-    <a href="mailto:john@gmail.com" target="_blank" class="card-link"
-      >john@gmail.com</a
-    >
-  </li>
-  <li class="list-group-item d-flex flex-row">
-    <p class="link-text pr-2">
-      <i class="fa-brands fa-github"></i> Github:
-    </p>
-    <a
-      href="https://github.com/John-Smith-Modded"
-      target="_blank"
-      class="card-link"
-      >John-Smith-Modded</a
-    >
-  </li>
-</ul>
-</div>`;
-
-  const internCard = `<div class="card employee-card m-2">
-<div class="card-body bg-intern">
-  <h5 class="card-title">Max</h5>
-  <p class="card-subtitle">
-    <i class="fa-solid fa-seedling"></i> Intern
-  </p>
-</div>
-<ul class="list-group list-group-flush p-2 list-container">
-  <li class="list-group-item">ID: 62</li>
-  <li class="list-group-item d-flex flex-row">
-    <p class="link-text pr-2">
-      <i class="fa-solid fa-envelope"></i> Email:
-    </p>
-    <a
-      href="mailto:max0215@gmail.com"
-      target="_blank"
-      class="card-link"
-      >max@gmail.com</a
-    >
-  </li>
-  <li class="list-group-item d-flex flex-row">
-    <p class="link-text pr-2">
-      <i class="fa-solid fa-graduation-cap"></i> School:
-    </p>
-    <p class="link-text">University of Nottingham</p>
-  </li>
-</ul>
-</div>`;
-
-  const createManagerHtml = (manager) => {
-    const managerName = manager.name;
-    const managerId = manager.id;
-    const managerEmail = manager.email;
-    const managerOffice = manager.officeNumber;
-
-    const managerString = `<section
-        class="manager-container d-flex flex-column align-items-center separator"
-        id="manager-section"
-      >
-        <div class="card employee-card m-2">
-          <div class="card-body bg-manager">
-            <h5 class="card-title">${managerName}</h5>
-            <p class="card-subtitle">
-              <i class="fa-solid fa-people-roof"></i> Manager
-            </p>
-          </div>
-          <ul class="list-group list-group-flush p-2 list-container">
-            <li class="list-group-item">ID: ${managerId}</li>
-            <li class="list-group-item d-flex flex-row">
-              <p class="link-text pr-2">
-                <i class="fa-solid fa-envelope"></i> Email:
-              </p>
-              <a href="mailto:bob@gmail.com" target="_blank" class="card-link"
-                >${managerEmail}</a
-              >
-            </li>
-            <li class="list-group-item">
-              <i class="fa-solid fa-door-closed"></i> Office number: ${managerOffice}
-            </li>
-          </ul>
-        </div>
-      </section>`;
-
-    return managerString;
-  };
-
-  const createEngineerHtml = () => {
-    const section = `<section
-    class="engineer-container d-flex flex-row flex-wrap justify-content-around align-items-center separator"
-    id="engineer-section"
-  >
-  </section>`;
-  };
-
-  const createInternHtml = () => {
-    const section = `<section
-      class="intern-container d-flex flex-row flex-wrap justify-content-around align-items-center"
-      id="intern-section"
-    >
-    </section>`;
-  };
-
+  //static string for html head section
   const headHtml = `<head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -146,32 +25,159 @@ const generateHtml = (manager, team) => {
   <link rel="stylesheet" href="./assets/css/styles.css" />
 </head>`;
 
+  //create string for header
   const headerHtml = `<header class="jumbotron jumbotron-fluid header-title mb-0">
   <div class="container">
-    <h1 class="display-4 text-center">${teamName}</h1>
+    <h1 class="display-4 text-center">${manager.team}</h1>
   </div>
 </header>`;
 
+  //create string for manager section
+  const createManagerHtml = (manager) => {
+    return `<section
+        class="manager-container d-flex flex-column align-items-center separator"
+        id="manager-section">
+        <div class="card employee-card m-2">
+          <div class="card-body bg-manager">
+            <h5 class="card-title">${manager.name}</h5>
+            <p class="card-subtitle">
+              <i class="fa-solid fa-people-roof"></i> Manager
+            </p>
+          </div>
+          <ul class="list-group list-group-flush p-2 list-container">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item d-flex flex-row">
+              <p class="link-text pr-2">
+                <i class="fa-solid fa-envelope"></i> Email:
+              </p>
+              <a href="mailto:bob@gmail.com" target="_blank" class="card-link"
+                >${manager.email}</a
+              >
+            </li>
+            <li class="list-group-item">
+              <i class="fa-solid fa-door-closed"></i> Office number: ${manager.officeNumber}
+            </li>
+          </ul>
+        </div>
+      </section>`;
+  };
+
+  //create string for engineer section
+  const createEngineerHtml = () => {
+    const createEngineerCardHtml = (each) => {
+      return `<div class="card employee-card m-2">
+    <div class="card-body bg-engineer">
+      <h5 class="card-title">${each.name}</h5>
+      <p class="card-subtitle">
+        <i class="fa-solid fa-brain"></i> Engineer
+      </p>
+    </div>
+    <ul class="list-group list-group-flush p-2 list-container">
+      <li class="list-group-item">ID: ${each.id}</li>
+      <li class="list-group-item d-flex flex-row">
+        <p class="link-text pr-2">
+          <i class="fa-solid fa-envelope"></i> Email:
+        </p>
+        <a href="mailto:${each.email}" target="_blank" class="card-link"
+          >${each.email}</a
+        >
+      </li>
+      <li class="list-group-item d-flex flex-row">
+        <p class="link-text pr-2">
+          <i class="fa-brands fa-github"></i> Github:
+        </p>
+        <a
+          href="https://github.com/${each.githubUsername}"
+          target="_blank"
+          class="card-link"
+          >${each.githubUsername}</a
+        >
+      </li>
+    </ul>
+    </div>`;
+    };
+
+    const engineers = team.filter((employee) => employee.role === "engineer");
+    return engineers.length === 0
+      ? `<div class="warning-message alert-warning w-100 mt-2 mb-2 text-center">
+    No engineers
+  </div>`
+      : `<section
+    class="engineer-container d-flex flex-row flex-wrap justify-content-around align-items-center separator"
+    id="engineer-section">
+    ${engineers.map(createEngineerCardHtml).join()}
+  </section>`;
+  };
+
+  //create string for intern section
+  const createInternHtml = () => {
+    const createInternCardHtml = (each) => {
+      return `<div class="card employee-card m-2">
+          <div class="card-body bg-intern">
+            <h5 class="card-title">${each.name}</h5>
+            <p class="card-subtitle">
+              <i class="fa-solid fa-seedling"></i> Intern
+            </p>
+          </div>
+          <ul class="list-group list-group-flush p-2 list-container">
+            <li class="list-group-item">ID: ${each.id}</li>
+            <li class="list-group-item d-flex flex-row">
+              <p class="link-text pr-2">
+                <i class="fa-solid fa-envelope"></i> Email:
+              </p>
+              <a
+                href="mailto:${each.email}"
+                target="_blank"
+                class="card-link"
+                >${each.email}</a
+              >
+            </li>
+            <li class="list-group-item d-flex flex-row">
+              <p class="link-text pr-2">
+                <i class="fa-solid fa-graduation-cap"></i> School:
+              </p>
+              <p class="link-text">${each.school}</p>
+            </li>
+          </ul>
+          </div>`;
+    };
+
+    const interns = team.filter((employee) => employee.role === "intern");
+    return interns.length === 0
+      ? `<div class="warning-message alert-warning w-100 mt-2 mb-2 text-center">
+    No interns
+  </div>`
+      : `<section
+      class="intern-container d-flex flex-row flex-wrap justify-content-around align-items-center"
+      id="intern-section">
+      ${interns.map(createInternCardHtml).join()}
+    </section>`;
+  };
+
+  //static string for footer
+  const footerHtml = `<footer class="footer p-3">
+  <div class="footer-text w-100 text-center">&copy; 2022 Copyright</div>
+</footer>`;
+
+  //call to generate the section strings
   const managerHtml = createManagerHtml();
   const engineerHtml = createEngineerHtml();
   const internHtml = createInternHtml();
 
+  //return the overall string
   return ` <!DOCTYPE html>
   <html>
   ${headHtml}
   <body>
-  ${headHtml}
+  ${headerHtml}
   <main class="main" id="main">
   ${managerHtml}
   ${engineerHtml}
   ${internHtml}
   </main>
-    <footer class="footer p-3">
-      <div class="footer-text w-100 text-center">&copy; 2022 Copyright</div>
-    </footer>
-    <script src="./assets/js/script.js" async defer></script>
+  ${footerHtml}
   </body>
-</html>
+    </html>
   `;
 };
 
