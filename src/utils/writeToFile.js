037b62dcc6880881cb17e599d7c125bfd3ebea76
Amelie Pira <amelie.pira@gmail.com>
2022-06-12 16:09:37 +0100

    added change-case module for filename format

--- src/utils/writeToFile.js
+++ src/utils/writeToFile.js
@@ -1,11 +1,15 @@
-// requiring the fs module from Node.js core
+//requiring the fs module from Node.js core
 const fs = require("fs");
 //requiring the path module from Node.js core
 const path = require("path");
+//requiring the change-case module to format file name
+const { paramCase } = require("change-case");
 
+//function to write string to file with given filename
 const writeToFile = (fileName, string) => {
   try {
-    const filepath = path.join(__dirname, "../dist", `${fileName}.html`);
+    const filename = paramCase(fileName);
+    const filepath = path.join(__dirname, "../dist", `${filename}.html`);
     // write data to new file
     fs.writeFileSync(filepath, string);
   } catch (error) {
